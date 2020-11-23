dde623db93c5165d6cd19954b0604195f70306a1
Elsa-R <56952607+Elsa-R@users.noreply.github.com>
2020-11-23 14:33:26 +0100

    Add files via upload

--- public/js/charts/pieChart_armeLegale/vis_pie.js
+++ public/js/charts/pieChart_armeLegale/vis_pie.js
@@ -1,40 +1,3 @@
-var data = [
-    {"platform": "Android","percentage": 30},
-    {"platform": "Windows","percentage": 70},
-];
-
-var acolor = ["#1A5276","#9A7D0A"];
-
-var svgWidth = 500, svgHeight = 300, radius = Math.min(svgWidth, svgHeight) /2;
-var svg = d3.select('svg')
-    .attr("width",svgWidth)
-    .attr("height", svgHeight);
-
-var g = svg.append("g")
-    .attr("transform", "translate(" + radius + ", " + radius + ")");
-
-//var color = d3.scale.ordinal()(d3.schemeCategory10);
-
-var pie = d3.layout.pie().value(function(d){
-    return d.percentage;
-});
-
-var path = d3.svg.arc()
-    .outerRadius(radius)
-    .innerRadius(0);
-
-var arc = g.selectAll("arc")
-    .data(pie(data))
-    .enter()
-    .append("g");
-
-
-arc.append("path")
-.attr("d",path)
-.attr("fill", function(d, i){return acolor[i];});
-
-
-
 // IMPORT DATA
 //d3.dsv(';')("datasets/dataFinalUS.csv", function(data) {
 
@@ -43,3 +6,64 @@ arc.append("path")
   //  var pie = d3.pie()
   //  console.log(pie(data))
 //});
+
+
+
+var width = 960,
+    height = 500,
+    // find the min of width and height and devided by 2
+    radius = Math.min(width, height) / 2;
+
+// Scales are functions that map from an input domain to an output range.  Ordinal scales have a discrete domain, such as a set of names or categories. 
+// from: https://github.com/mbostock/d3/wiki/Ordinal-Scales
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+// Constructs a new arc generator with the default innerRadius-, outerRadius-, startAngle- and endAngle-accessor functions.  
+// from: https://github.com/mbostock/d3/wiki/SVG-Shapes#arc
+var arc = d3.svg.arc()
+    // the outer radius of the pie chart.
+    .outerRadius(radius - 10)
+    // the inner radius of the pie chart, set 0 for now
+    .innerRadius(0);
+
+
+// Constructs a new pie function
+var pie = d3.layout.pie()
+    // not sorting
+    .sort(null)
+    // set the pie chart value to population.
+    .value(function(d) { return d.population; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+d3.csv("datasets/dataArme.csv", function(error, data) {
+
+  // convert all population to integer
+  data.forEach(function(d) {
+    d.population = +d.population;
+  });
+
+  // append a group
+  var g = svg.selectAll(".arc")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  // append path, the pie for each legal
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.legal); });
+
+  // add text
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.data.legal; });
+
+});
